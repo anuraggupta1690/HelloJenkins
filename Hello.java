99560c40861c7442624653ec0e472b62b87cabc2
anuraggupta1690 <anuraggupta16902gmail.com>
2018-10-26 13:47:02 +0530

    adding to git

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+
+public static void main(String args[]){
+
+for(int i=1;i<=10;i++){
+
+
+System.out.println("hello");
+
+try{
+Thread.sleep(1000);}
+catch(Exception e){System.out.println(e);}
+}
+}
+}
